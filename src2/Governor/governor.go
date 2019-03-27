315a31e1fbfe8b39130aa313991e2b2248b7ca2a
Tora Haga <toragh@stud.ntnu.no>
2019-03-27 14:00:36 +0100

    updates

--- src2/Governor/governor.go
+++ src2/Governor/governor.go
@@ -12,10 +12,7 @@ func SetLights(){
 
 }
 
-//SPØRSMÅL:
-// Nå fjernes ordre fra queue og HallRequests på to måter. Enten at lokalPeer sletter når den har utført ordre i en etasje
-// eller at governor sjekker om en heis står i state DOOR_OPEN og sletter ordren derfra. Kan det skape trøbbel?
-// Må tenke litt gjennom rekkefølgen vi gjør ting: som når vi distribuerer/kalkulerer optimal heis for ordre og når vi sender ut info
+//Huske på: Vi MÅ sørge for at alle har den nyeste informasjonen når det gjøres en distribuering!
 
 
 func SpamGlobalState(){ //Update and broadcast latest globalState from one peer
@@ -35,32 +32,36 @@ func UpdateGlobalState(PeerUpdate chan Config.Elev){
 		//Watchdog <- globalState, after 6 sec without activity in network, peer takes ownership of all global orders
 		Update.c <- GlobalState
 		select{
-			case Update <- StateUpdate.c //StateUpdate from other Peer
+			case Update <- StateUpdate //StateUpdate from other Peer
 			OutsideElev:= Update.Map[Update.Id]
+
+			// 1. Set our info about the OutsideElev to the update from OutsideElev
+			Gstate.Map[Update.Id] = OutsideElev
+			// 2. If OutsideElev in state = DOOR_OPEN, remove orders in that floor
 			if OutsideElev.State == DOOR_OPEN{
-					//Set HallRequests UP and DOWN in that floor to zero
-					GState.HallRequests[OutsideElev.Floor][0] = 0
-					GState.HallRequests[OutsideElev.Floor][1] = 0
-					//Set HallOrders in other elevs queue to zero
-					for _,elev := range GState.Map{
-						elev.Queue[OutsideElev.Floor][0] = 0
-						elev.Queue[OutsideElev.Floor][1] = 0
-						}
+					for button:= 0;  button < Config.NumButtons - 1 ; ++ {
+						GState.HallRequests[peerUpdate.Floor][button] = false
+					}
 			}
-			//Copy Caborders for OutsideElev to own map
-			GState[Update.Id].Cab = Outside.cab // denne må fikses, cab = slice Queue
-			//compare, OR global hallmat
-				//if 1 when initially 0, distribute order to best elev by also setting that queue=1 for that elev in own globalState
+			// 3. Fylle lokal HallRequests med de evt nye knappetrykkene fra oppdateringen
+			// Ta vare på disse knappetrykkene så de kan bli distribuert
+			var newButton Config.ButtonEvent
+			for floor:= 0;  floor < Config.NumFloors; ++ {
+				for button:= 0;  button < Config.NumButtons; ++ {
+					if Update.HallRequests[floor][button] == true {
+						GState.HallRequests[floor][button] = true
+						newButton.Floor = floor
+						newButton.Button = button
+						// Distribuere denne nye knappen, evt legge den til i en liste over knapper som må distrubueres
+					}
+				}
+			}
+
 
 			case Id <- lostElev.c
 			//for req in elev[ID] distribute orders
 			//delete ID from map in global state
 			case newOrder:= <- elevio.ButtonPushed //sent when ordefinished or new button
-			//Update hallmat
-			//distribute order to best elev by also setting that req=1 for that elev in own globalState
-			//if internal -- <- ping fsm
-
-
 			// 1. Sjekke om cab eller hall-Button. Oppdatere cab eller oppdatere global HallRequests OG sende ut
 			// denne oppdaterte Gstaten FØR optimalisering? Slik at alle optimaliserer likt? Eller er ikke dette nødvendig
 			if newOrder.Button == 2{
@@ -70,10 +71,10 @@ func UpdateGlobalState(PeerUpdate chan Config.Elev){
 				GState.HallRequests[newOrder.Floor][newOrder.Button] = true
 				// send ut ny update til alle?
 
-				// 2. Kjøre en optimize/cost og distribuere ordren, dvs legge den inn i den med lavest kost sin lokale queue
+				// 2. Kjøre en optimize/cost og distribuere ordren til oss selv om vi får den
 				keyBestElevator:= ChooseElevator(Gstate.Map, newOrder)
-				Gstate.Map[keyBestElevator].Queue[newOrder.Floor][newOrder.Button]
 				if keyBestElevator == "minId" {
+					Gstate.Map["minId"].Queue[newOrder.Floor][newOrder.Button]
 					PeerFSM.pingFromGov <- 1
 				}
 			}
@@ -81,7 +82,6 @@ func UpdateGlobalState(PeerUpdate chan Config.Elev){
 			// 3. Gi en ping til lokal om at det har skjedd en update (gjør dette underveis)
 
 			case peerUpdate:= <- PeerFSM.LocalStateUpdate 		//comes from local, Husk: dette er en Elev struct
-
 			// 1. Bytte ut hele min egen elevState/peer med denne updaten
 			Gstate.Map["minId"] = peerUpdate
 
@@ -89,28 +89,6 @@ func UpdateGlobalState(PeerUpdate chan Config.Elev){
 			if peerUpdate.State == Config.DOOR_OPEN {
 				for button:= 0;  button < Config.NumButtons - 1 ; ++ {
 					GState.HallRequests[peerUpdate.Floor][button] = false
-				}
-			}
-
-			// Fra tirsdag:
-			// 2. Oppdatere global HallRequests
-			for floor:= 0;  floor < Config.NumFloors; ++ {
-				for button:=0;  < Config.NumButtons-1; ++ {
-					if peerUpdate.Queue[floor][button]==false &&  Gstate.HallRequests[floor][button]==true{
-						Gstate.HallRequests[floor][button]==false
-					}
-				}
-			}
-			// 3. Oppdatere alles lokale HallRequests/Queue
-			// Oppdatere innebærer å fjerne ordre som er fjernet fra lokalHall i update
-			for id := 0;  id < Config.NumOfElev; id++ {
-				HallRequestsOfId:= Gstate.Map[id]
-				for floor:= 0;  floor < Config.NumFloors; ++ {
-					for button:=0;  < Config.NumButtons-1; ++ {
-						if peerUpdate.Queue[floor][button]==false &&  HallRequestsOfId.Queue[floor][button]==true{
-							HallRequestsOfId.Queue[floor][button]==false
-						}
-					}
 				}
 			}
 		}
