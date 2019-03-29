d3ca71466b5e4e215a33116648f5d53634b046a1
Sirihhe <larslsa@stud.ntnu.no>
2019-03-29 17:49:15 +0100

    Establish log branch

--- src2/Governor/governor.go
+++ src2/Governor/governor.go
@@ -1,11 +1,18 @@
 package Governor
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
 	"../Config"
-//	"../elevio"
-	"../PeerFSM"
+
+	//	"../elevio"
 	"time"
+
+	"../PeerFSM"
 	"../network/bcast"
 )
 
@@ -13,8 +20,7 @@ import (
 var latestState Config.GlobalState
 var StateUpdate Config.GlobalState
 
-
-func SpamGlobalState(gchan Config.GovernorChannels){ //Update and broadcast latest globalState from one peer
+func SpamGlobalState(gchan Config.GovernorChannels) { //Update and broadcast latest globalState from one peer
 	//This is the global state
 	ticker := time.Tick(500 * time.Millisecond)
 	transmitNet := make(chan Config.GlobalState)
@@ -23,39 +29,37 @@ func SpamGlobalState(gchan Config.GovernorChannels){ //Update and broadcast late
 	// transmitTest := make(chan map[string]Config.Elev)
 	// go bcast.Transmitter(16700, transmitTest)
 
-	for{
-		select{
-			case <- ticker: // ticker
-				//fmt.Println(t)
-				//fmt.Println("Transmit latestState", latestState)
-				transmitNet <- latestState //sending latest state to network
-				//fmt.Println("Transmitting GlobalState from ID: ", latestState.Id)
+	for {
+		select {
+		case <-ticker: // ticker
+			//fmt.Println(t)
+			//fmt.Println("Transmit latestState", latestState)
+			transmitNet <- latestState //sending latest state to network
+			//fmt.Println("Transmitting GlobalState from ID: ", latestState.Id)
 
-				// fmt.Println("Transmit Map", latestState.Map)
-				// transmitTest <- latestState.Map
+			// fmt.Println("Transmit Map", latestState.Map)
+			// transmitTest <- latestState.Map
 
-			case newUpdate:= <- gchan.InternalState:
-				latestState = newUpdate
-				//fmt.Println("latestState:", latestState)
+		case newUpdate := <-gchan.InternalState:
+			latestState = newUpdate
+			//fmt.Println("latestState:", latestState)
 		}
 	}
 }
 
-func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels, id string, GState Config.GlobalState){
-	    //ticker := time.NewTicker(500 * time.Millisecond)
-			distribute := false
-			var hallOrder Config.ButtonEvent
+func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels, id string, GState Config.GlobalState) {
+	//ticker := time.NewTicker(500 * time.Millisecond)
+	distribute := false
+	var hallOrder Config.ButtonEvent
 
-
-
-	for{
+	for {
 		//Watchdog <- globalState, after 6 sec without activity in network, peer takes ownership of all global orders
 		gchan.InternalState <- GState
 		fmt.Println("distribute: ", distribute)
-		if distribute{
+		if distribute {
 			fmt.Println("inside distribute")
 			//distribuere
-			keyBestElevator:= ChooseElevator(GState.Map, hallOrder, GState)
+			keyBestElevator := ChooseElevator(GState.Map, hallOrder, GState)
 			fmt.Println("Distributing to: ", keyBestElevator)
 			if keyBestElevator == GState.Id {
 				var x = GState.Map[GState.Id]
@@ -67,31 +71,30 @@ func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels
 			distribute = false
 		}
 
-		select{
-			// case <- ticker.C:
-			// 		FSMchan.PingFromGov <- GState
-			case Update:=  <- gchan.ExternalState: //StateUpdate from other Peer
+		select {
+		// case <- ticker.C:
+		// 		FSMchan.PingFromGov <- GState
+		case Update := <-gchan.ExternalState: //StateUpdate from other Peer
 			//fmt.Println("external state: ", Update.Id)
-			OutsideElev:= Update.Map[Update.Id]
-
+			OutsideElev := Update.Map[Update.Id]
 
 			// 1. Set our info about the OutsideElev to the update from OutsideElev
 			GState.Map[Update.Id] = OutsideElev
 			// 2. If OutsideElev in state = DOOR_OPEN, remove orders in that floor
-			if OutsideElev.State == Config.DOOR_OPEN{
-					for button:= 0;  button < Config.NumButtons - 1 ; button++ {
-						GState.HallRequests[OutsideElev.Floor][button] = false
-					}
+			if OutsideElev.State == Config.DOOR_OPEN {
+				for button := 0; button < Config.NumButtons-1; button++ {
+					GState.HallRequests[OutsideElev.Floor][button] = false
+				}
 			}
 			// 3. Fylle lokal HallRequests med de evt nye knappetrykkene fra oppdateringen
 			// Ta vare på disse knappetrykkene så de kan bli distribuert
 			newOrder := Config.ButtonEvent{}
-			for floor:= 0;  floor < Config.NumFloors; floor++ {
-				for button:= 0;  button < Config.NumButtons-1; button++ {
-					if Update.HallRequests[floor][button] && !GState.HallRequests[floor][button]{
+			for floor := 0; floor < Config.NumFloors; floor++ {
+				for button := 0; button < Config.NumButtons-1; button++ {
+					if Update.HallRequests[floor][button] && !GState.HallRequests[floor][button] {
 						GState.HallRequests[floor][button] = true
 						newOrder = Config.ButtonEvent{floor, Config.ButtonType(button)}
-						keyBestElevator:= ChooseElevator(GState.Map, newOrder, GState)
+						keyBestElevator := ChooseElevator(GState.Map, newOrder, GState)
 						fmt.Println("Distributing to: ", keyBestElevator)
 						if keyBestElevator == GState.Id {
 							var x = GState.Map[GState.Id]
@@ -105,18 +108,16 @@ func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels
 			}
 			PeerFSM.Lights(GState, GState.Map[id], id)
 
-
-
-		case Id:= <- gchan.LostElev:
-			lostElev:= GState.Map[Id]
+		case Id := <-gchan.LostElev:
+			lostElev := GState.Map[Id]
 			delete(GState.Map, Id)
 			newOrder := Config.ButtonEvent{}
 
-			for floor:= 0;  floor < Config.NumFloors; floor++ {
-				for button:=0;  button < Config.NumButtons-1; button++ {
-					if lostElev.Queue[floor][button]{
+			for floor := 0; floor < Config.NumFloors; floor++ {
+				for button := 0; button < Config.NumButtons-1; button++ {
+					if lostElev.Queue[floor][button] {
 						newOrder = Config.ButtonEvent{floor, Config.ButtonType(button)}
-						keyBestElevator:= ChooseElevator(GState.Map, newOrder, GState)
+						keyBestElevator := ChooseElevator(GState.Map, newOrder, GState)
 						if keyBestElevator == GState.Id {
 							var x = GState.Map[GState.Id]
 							x.Queue[hallOrder.Floor][hallOrder.Button] = true
@@ -127,31 +128,30 @@ func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels
 				}
 			}
 
-		case hallOrder= <- gchan.AddHallOrder:
+		case hallOrder = <-gchan.AddHallOrder:
 			//fmt.Println("Hall", GState.HallRequests)
 			fmt.Println("Got Hall from Peer")
-				GState.HallRequests[hallOrder.Floor][hallOrder.Button] = true
-							fmt.Println("set distrute to true")
-				distribute = true
-		case cabOrderFloor:= <- FSMchan.AddCabOrderGov:
+			GState.HallRequests[hallOrder.Floor][hallOrder.Button] = true
+			fmt.Println("set distrute to true")
+			distribute = true
+		case cabOrderFloor := <-FSMchan.AddCabOrderGov:
 			fmt.Println("Got Hall from Peer")
-				var x = GState.Map[GState.Id]
-				x.Queue[cabOrderFloor][2] = true
-				GState.Map[GState.Id] = x
-				fmt.Println("Got Hall from Peer")
-				FSMchan.PingFromGov <- GState
-				fmt.Println("Sent update to Peer")
-		case update:= <- FSMchan.LocalStateUpdate:
-			tempQueue:= GState.Map[GState.Id].Queue
+			var x = GState.Map[GState.Id]
+			x.Queue[cabOrderFloor][2] = true
+			GState.Map[GState.Id] = x
+			fmt.Println("Got Hall from Peer")
+			FSMchan.PingFromGov <- GState
+			fmt.Println("Sent update to Peer")
+		case update := <-FSMchan.LocalStateUpdate:
+			tempQueue := GState.Map[GState.Id].Queue
 			GState.Map[GState.Id] = update
 			var x = GState.Map[GState.Id]
 			x.Queue = tempQueue
 			GState.Map[GState.Id] = x
 
-
 			//Dersom staten som oppdateringen har(alstå FSM-state) er DOOR_OPEN, fjern hall i denne etasjen
 			if update.State == Config.DOOR_OPEN {
-				for button:= 0;  button < Config.NumButtons - 1 ; button++ {
+				for button := 0; button < Config.NumButtons-1; button++ {
 					GState.HallRequests[update.Floor][button] = false
 				}
 				var x = GState.Map[GState.Id]
@@ -161,14 +161,14 @@ func UpdateGlobalState(gchan Config.GovernorChannels, FSMchan Config.FSMChannels
 				GState.Map[GState.Id] = x
 				fmt.Println("Q.D:", GState.Map[GState.Id].Queue)
 			}
-				PeerFSM.Lights(GState, GState.Map[id], id)
+			PeerFSM.Lights(GState, GState.Map[id], id)
 		}
 	}
 }
 
-func NetworkState(gchan Config.GovernorChannels){
-	seen:= make(map[string]time.Time)
-	timeOut := Config.TIMEOUT*time.Second
+func NetworkState(gchan Config.GovernorChannels) {
+	seen := make(map[string]time.Time)
+	timeOut := Config.TIMEOUT * time.Second
 
 	receiveNet := make(chan Config.GlobalState)
 	go bcast.Receiver(16700, receiveNet)
@@ -176,24 +176,23 @@ func NetworkState(gchan Config.GovernorChannels){
 	// receiveTest := make(chan map[int]Config.Elev)
 	// go bcast.Receiver(16701, receiveTest)
 
-
-	for{
-		select{
-		case StateUpdate= <- receiveNet:
+	for {
+		select {
+		case StateUpdate = <-receiveNet:
 
 			if latestState.Id != StateUpdate.Id {
-			//fmt.Println("Receive state: ", StateUpdate)
-			seen[StateUpdate.Id] = time.Now()
-			gchan.ExternalState <- StateUpdate
-			//fmt.Println("Received external state from ID:", StateUpdate.Id)
-			//fmt.Println(StateUpdate)
-		}
+				//fmt.Println("Receive state: ", StateUpdate)
+				seen[StateUpdate.Id] = time.Now()
+				gchan.ExternalState <- StateUpdate
+				//fmt.Println("Received external state from ID:", StateUpdate.Id)
+				//fmt.Println(StateUpdate)
+			}
 		// case Update:= <- receiveTest:
 		// 	fmt.Println("recTest: ", Update)
 		default:
-			for k, v := range seen{
-				t:= time.Now().Sub(v)
-				if (t > timeOut){
+			for k, v := range seen {
+				t := time.Now().Sub(v)
+				if t > timeOut {
 					gchan.LostElev <- k
 					delete(seen, k)
 				}
@@ -202,13 +201,64 @@ func NetworkState(gchan Config.GovernorChannels){
 	}
 }
 
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-func GovernorInit(GState Config.GlobalState, id string)Config.GlobalState  {
-	Queue1:= [4][3]bool{{false,false,false},{false,false,false},{false,false,false} ,{false,false,false}}
-	ElevState := Config.Elev{Config.UNKNOWN, Config.MD_Up, 0, Queue1}
-	GState.Map = make(map[string]Config.Elev)
-	GState.HallRequests= [4][2]bool{{false,false},{false,false},{false,false} ,{false,false}}
-	GState.Id = id
-	GState.Map[GState.Id] = ElevState
-	return GState
+	return (err != nil)
+}
+
+func GovernorInit(GState Config.GlobalState, id string) Config.GlobalState {
+
+	//Check for existing backup filename
+	//if file exists
+	//Load data from it
+	//Else creat empty data files
+	filename := "backupfile.txt"
+	var _, err1 = os.Stat(filename)
+	if os.IsNotExist(err1) {
+		var file, err1 = os.Create(filename)
+		isError(err1)
+		defer file.Close()
+		Queue1 := [4][3]bool{{false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}}
+		ElevState := Config.Elev{Config.UNKNOWN, Config.MD_Up, 0, Queue1}
+		GState.Map = make(map[string]Config.Elev)
+		GState.HallRequests = [4][2]bool{{false, false}, {false, false}, {false, false}, {false, false}}
+		GState.Id = id
+		GState.Map[GState.Id] = ElevState
+		// Write Log to file in json-format
+		GStatejason, _ := json.MarshalIndent(GState, "", " ")
+		_ = ioutil.WriteFile(filename, GStatejason, 0644)
+		return GState
+	} else {
+		var readFile, err2 = os.OpenFile(filename, os.O_RDWR, 0644)
+		isError(err2)
+		defer readFile.Close()
+
+		var GStatetext = make([]byte, 1024)
+
+		for {
+			_, err3 := readFile.Read(GStatetext)
+
+			// Break if finally arrived at end of file
+			if err3 == io.EOF {
+				break
+			}
+
+			// Break if error occured
+			if err3 != nil && err3 != io.EOF {
+				isError(err3)
+				break
+			}
+		}
+		// Undo json
+		var GStateByte = []byte(GStatetext)
+		//fmt.Println(new)
+		error := json.Unmarshal(GStateByte, &GState)
+		if error != nil {
+			fmt.Println("error:", error)
+		}
+		return GState
+	}
 }
