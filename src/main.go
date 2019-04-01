62b83dd694847d27621b70eeea955d5e2a397f72
EdvardGrodem <edvard.grodem@gmail.com>
2019-04-01 10:50:47 +0200

    Cleaned up main

--- src/main.go
+++ src/main.go
@@ -2,61 +2,45 @@ package main
 
 import (
 	"fmt"
+	"flag"
 
 	"./DataStructures"
 	"./Governor"
 	"./PeerFSM"
 	"./elevio"
-
-	//"time"
-	"flag"
-	//"os"
-	//"./Peertest"
 )
 
 func main() {
-	// Setting Id
+
+	// Setting id to the individual elevator
 	var id string
 	flag.StringVar(&id, "id", "", "id of this peer")
 
-	// var port string
-	// flag.StringVar(&port, "port", "", "port of this peer")
 	flag.Parse()
-
 	elevio.Init("localhost:15657", 4)
 	fmt.Println("ID: ", id)
 
+//	Define a unique backup file name
 	DataStructures.Backupfilename = "backup" + id + ".txt"
-
-	// if id == "" {
-	//     localIP, err := localip.LocalIP()
-	//     if err != nil {
-	//         fmt.Println(err)
-	//         localIP = "DISCONNECTED"
-	//     }
-	//     id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
-	// }
 
 	var GState DataStructures.GlobalState
 	GState = Governor.GovernorInit(GState, id)
-	//fmt.Println("Gstate init: ", GState)
 
 	PeerFSMChannels := DataStructures.FSMChannels{
 		CurrentFloor:     make(chan int),
 		LocalStateUpdate: make(chan DataStructures.Elev, 10),
 		PingFromGov:      make(chan DataStructures.GlobalState),
 		ButtonPushed:     make(chan DataStructures.ButtonEvent),
-		//AddCabOrder:        make(chan int),
-		AddCabOrderGov: make(chan int),
+		AddCabOrderGov: 	make(chan int),
 	}
 
 	GovernorChannels := DataStructures.GovernorChannels{
-		InternalState: make(chan DataStructures.GlobalState),
-		ExternalState: make(chan DataStructures.GlobalState),
-		LostElev:      make(chan string),
-		AddHallOrder:  make(chan DataStructures.ButtonEvent),
-		UpdatefromSpam: make(chan DataStructures.GlobalState),
-		Watchdog:		make(chan int),
+		InternalState: 		make(chan DataStructures.GlobalState),
+		ExternalState: 		make(chan DataStructures.GlobalState),
+		LostElev:      		make(chan string),
+		AddHallOrder:  		make(chan DataStructures.ButtonEvent),
+		UpdatefromSpam: 	make(chan DataStructures.GlobalState),
+		Watchdog:					make(chan int),
 	}
 
 	go Governor.UpdateGlobalState(GovernorChannels, PeerFSMChannels, id, GState)
@@ -66,5 +50,6 @@ func main() {
 	go PeerFSM.FSM(GovernorChannels, PeerFSMChannels, id, GState)
 
 	for {
+		//Run elevator
 	}
 }
